70cc82508ad49c070918eb91012068da37c494a9
Manuel <pedraza.sanchez.manuel.alejandro@gmail.com>
2024-08-07 15:00:25 -0400

    Remade unsuccessful unit test

--- src/main/java/com/example/HelloActions.java
+++ src/main/java/com/example/HelloActions.java
@@ -1,7 +1,11 @@
 package com.example;
 
 public class HelloActions {
+  public static String sayHello(){
+    return "Hello, GitHub Actions!";
+  }
+
   public static void main(String[] args) {
-    System.out.println("Hello, GitHub Actions!");
+    System.out.println(sayHello());
   }
 }
--- src/test/java/com/example/HelloActionsTest.java
+++ src/test/java/com/example/HelloActionsTest.java
@@ -10,9 +10,10 @@ public class HelloActionsTest {
   public void testHelloActions() {
     ByteArrayOutputStream outContent = new ByteArrayOutputStream();
     System.setOut(new PrintStream(outContent));
-    HelloActions.main(new String[]{});
-    assertEquals("Hello, GitHub Actions!\n", outContent.toString());
+    HelloActions.main(new String[] {});
+    // assertEquals(1, 1);
+    // assertEquals("Hello, GitHub Actions!\n", outContent.toString());
+    assertEquals("Hello, GitHub Actions!", HelloActions.sayHello());
     System.setOut(System.out);
   }
 }
-
